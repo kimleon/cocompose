ee1704d3cf3e4957e12c55f4adde652852e45d04
Lisandro Jimenez <jimenezl@mit.edu>
2015-11-19 13:15:43 -0500

    add sheet schema

--- models/sheet.js
+++ models/sheet.js
@@ -1,2 +1,13 @@
 var mongoose = require("mongoose");
 
+var noteSchema = mongoose.Schema({
+  start: Number,
+  duration: Number,
+  pitch: String
+});
+
+var sheetSchema = mongoose.Schema({
+  notes: [noteSchema],
+  creator: String,
+  collaborators: String
+});
