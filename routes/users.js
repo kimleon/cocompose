38d78810880334de5a61161fc6c6cbc16e756fcd
kimleon <kimleon@mit.edu>
2015-11-19 13:41:40 -0500

    files for sheets and users, might need notes

--- routes/users.js
+++ routes/users.js
@@ -1,5 +1,8 @@
 var express = require('express');
 var router = express.Router();
+var utils = require('../utils/utils');
+var User = require('../models/user');
+var Tweet = require('../models/sheet');
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
