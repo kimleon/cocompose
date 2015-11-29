db9cc14a41cb372078f02f5d440351eceec149ea
Lisandro Jimenez <jimenezl@mit.edu>
2015-11-29 06:19:08 -0500

    ui changes

--- public/javascripts/templates.js
+++ public/javascripts/templates.js
@@ -1,59 +1,59 @@
 (function() {
   var template = Handlebars.template, templates = Handlebars.templates = Handlebars.templates || {};
 templates['index'] = template({"compiler":[7,">= 4.0.0"],"main":function(container,depth0,helpers,partials,data) {
-    return "<div id=\"homepage\" class=\"allCenter\">\n  <h1>CoCompose</h1>\n  <p>You must be signed in to continue.</p>\n  <button id=\"signin-btn\" class='tealButton'>Sign in</button>\n  <button id=\"register-btn\" class='tealButton'>Register</button>\n</div>\n";
+    return "\n<div class=\"container\">\n  	<div class=\"page-header\">\n	  	<h1>CoCompose</h1>\n	</div>\n	  	<p>You must be signed in to continue.</p>\n		<button id=\"signin-btn\" class='btn btn-info'>Sign in</button>\n		<button id=\"register-btn\" class='btn btn-info'>Register</button>\n</div>\n";
 },"useData":true});
 templates['register'] = template({"1":function(container,depth0,helpers,partials,data) {
     var helper;
 
-  return "      "
-    + container.escapeExpression(((helper = (helper = helpers.error || (depth0 != null ? depth0.error : depth0)) != null ? helper : helpers.helperMissing),(typeof helper === "function" ? helper.call(depth0 != null ? depth0 : {},{"name":"error","hash":{},"data":data}) : helper)))
+  return "        "
+    + container.escapeExpression(((helper = (helper = helpers.error || (depth0 != null ? depth0.error : depth0)) != null ? helper : helpers.helperMissing),(typeof helper === "function" ? helper.call(depth0,{"name":"error","hash":{},"data":data}) : helper)))
     + "\n";
 },"compiler":[7,">= 4.0.0"],"main":function(container,depth0,helpers,partials,data) {
     var stack1;
 
-  return "<div id=\"register\" class=\"allCenter\">\n  <a href=\"#\" id=\"home-link\">Back to Home</a>\n  <h1>Register</h1>\n  <div class=\"error\">\n"
-    + ((stack1 = helpers["if"].call(depth0 != null ? depth0 : {},(depth0 != null ? depth0.error : depth0),{"name":"if","hash":{},"fn":container.program(1, data, 0),"inverse":container.noop,"data":data})) != null ? stack1 : "")
-    + "  </div>\n  <form id=\"register-form\" class=\"spaceInputs\">\n    <div>Username: <input type=\"text\" name=\"username\" required /></div>\n    <div>Password: <input type=\"password\" name=\"password\" required /></div>\n    <div>Confirm Password: <input type=\"password\" name=\"confirm\" required /></div>\n    <input type=\"submit\" />\n  </form>\n</div>\n";
+  return "\n\n<div class=\"container\">\n    <div class=\"page-header\">\n      <h1>CoCompose</h1>\n    </div>\n    <a href=\"#\" id=\"home-link\">Back to Home</a>\n    <h1>Register</h1>\n    <div class=\"error\">\n"
+    + ((stack1 = helpers["if"].call(depth0,(depth0 != null ? depth0.error : depth0),{"name":"if","hash":{},"fn":container.program(1, data, 0),"inverse":container.noop,"data":data})) != null ? stack1 : "")
+    + "    </div>\n    <form id=\"register-form\" class=\"form-group\">\n      <div>Username: <input type=\"text\" name=\"username\" class=\"form-control\" placeholder=\"Username\" required /></div> <br>\n      <div>Password: <input type=\"password\" name=\"password\" class=\"form-control\" placeholder=\"Password\" required /></div> <br>\n      <div>Confirm Password: <input type=\"password\" name=\"confirm\" class=\"form-control\" placeholder=\"Password\" required /></div>\n      <br>\n      <input type=\"submit\" class=\"btn btn-info\"/>\n    </form>\n</div>\n";
 },"useData":true});
 templates['sheet'] = template({"compiler":[7,">= 4.0.0"],"main":function(container,depth0,helpers,partials,data) {
-    var helper, alias1=depth0 != null ? depth0 : {}, alias2=helpers.helperMissing, alias3="function", alias4=container.escapeExpression;
+    var helper, alias1=helpers.helperMissing, alias2="function", alias3=container.escapeExpression;
 
   return "<div class=\"sheet\" data-sheet-id="
-    + alias4(((helper = (helper = helpers._id || (depth0 != null ? depth0._id : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"_id","hash":{},"data":data}) : helper)))
+    + alias3(((helper = (helper = helpers._id || (depth0 != null ? depth0._id : depth0)) != null ? helper : alias1),(typeof helper === alias2 ? helper.call(depth0,{"name":"_id","hash":{},"data":data}) : helper)))
     + ">\n  <a href=\"./sheets/"
-    + alias4(((helper = (helper = helpers._id || (depth0 != null ? depth0._id : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"_id","hash":{},"data":data}) : helper)))
+    + alias3(((helper = (helper = helpers._id || (depth0 != null ? depth0._id : depth0)) != null ? helper : alias1),(typeof helper === alias2 ? helper.call(depth0,{"name":"_id","hash":{},"data":data}) : helper)))
     + "\" class=\"go-to-sheet\">"
-    + alias4(((helper = (helper = helpers.name || (depth0 != null ? depth0.name : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"name","hash":{},"data":data}) : helper)))
+    + alias3(((helper = (helper = helpers.name || (depth0 != null ? depth0.name : depth0)) != null ? helper : alias1),(typeof helper === alias2 ? helper.call(depth0,{"name":"name","hash":{},"data":data}) : helper)))
     + "</a><button class=\"delete-sheet\"><i class=\"fa fa-trash fa-lg\"></i></button>\n</div>\n";
 },"useData":true});
 templates['sheets'] = template({"1":function(container,depth0,helpers,partials,data) {
     var stack1;
 
-  return ((stack1 = container.invokePartial(partials.sheet,depth0,{"name":"sheet","data":data,"indent":"\t\t  ","helpers":helpers,"partials":partials,"decorators":container.decorators})) != null ? stack1 : "")
-    + "		  <br>\n";
+  return ((stack1 = container.invokePartial(partials.sheet,depth0,{"name":"sheet","data":data,"indent":"\t\t\t  ","helpers":helpers,"partials":partials,"decorators":container.decorators})) != null ? stack1 : "")
+    + "			  <br>\n";
 },"compiler":[7,">= 4.0.0"],"main":function(container,depth0,helpers,partials,data) {
-    var stack1, helper, alias1=depth0 != null ? depth0 : {};
+    var stack1, helper;
 
-  return "<div id=\"sheets\">\n	<p>Welcome, "
-    + container.escapeExpression(((helper = (helper = helpers.currentUser || (depth0 != null ? depth0.currentUser : depth0)) != null ? helper : helpers.helperMissing),(typeof helper === "function" ? helper.call(alias1,{"name":"currentUser","hash":{},"data":data}) : helper)))
-    + " (<a href=\"#\" id=\"logout-link\">logout</a>)</p>\n	<h1>CoCompose Music Library</h1>\n	<div>\n      <div class=\"error\"></div>\n      <label for=\"new-sheet-input\">Add a new sheet:</label>\n      <input type=\"text\" id=\"new-sheet-input\" />\n      <button id=\"submit-new-sheet\" class='tealButton'>Add</button>\n    </div>\n    <div class=\"left\">\n		<h1>My Sheets</h1>\n\n"
-    + ((stack1 = helpers.each.call(alias1,(depth0 != null ? depth0.own_sheets : depth0),{"name":"each","hash":{},"fn":container.program(1, data, 0),"inverse":container.noop,"data":data})) != null ? stack1 : "")
-    + "	</div>\n\n	<div class=\"right\">\n		<h1>Sheets Shared With Me</h1>\n\n"
-    + ((stack1 = helpers.each.call(alias1,(depth0 != null ? depth0.collab_sheets : depth0),{"name":"each","hash":{},"fn":container.program(1, data, 0),"inverse":container.noop,"data":data})) != null ? stack1 : "")
-    + "	</div>\n</div>\n";
+  return "<div class=\"container\">\n  	<div class=\"page-header\">\n	<p>Welcome, "
+    + container.escapeExpression(((helper = (helper = helpers.currentUser || (depth0 != null ? depth0.currentUser : depth0)) != null ? helper : helpers.helperMissing),(typeof helper === "function" ? helper.call(depth0,{"name":"currentUser","hash":{},"data":data}) : helper)))
+    + " (<a href=\"#\" id=\"logout-link\">logout</a>)</p>\n	<h1>CoCompose Music Library</h1>\n	<div class=\"error\"></div>\n	<form class=\"form-horizontal\" role=\"form\">\n        <div class=\"form-group\">\n            <label for=\"new-sheet-input\" class=\"col-sm-2 control-label\">Add a new sheet:</label>\n            <div class=\"col-sm-4\">\n                <input type=\"text\" class=\"form-control\" id=\"new-sheet-input\" placeholder=\"Sheet Name\">\n            </div>\n            <div class=\"col-sm-2\">\n            	<button id=\"submit-new-sheet\" class='btn btn-info'>Add</button>\n            </div>\n        </div>\n    </form>\n	<!-- <div class=\"row\"> -->\n\n      <!-- <div class=\"form-group col-md-6\">\n\n	  	<label for=\"new-sheet-input\">Add a new sheet:</label>\n      	<input type=\"text\" id=\"new-sheet-input\" class=\"form-control\"/>\n      </div>\n      <div class=\"col-md-6\">\n      	<button id=\"submit-new-sheet\" class='btn btn-info'>Add</button>\n      </div> -->\n    <div class=\"row\">\n        <div class=\"col-md-6\">\n			<h1>My Sheets</h1>\n\n"
+    + ((stack1 = helpers.each.call(depth0,(depth0 != null ? depth0.own_sheets : depth0),{"name":"each","hash":{},"fn":container.program(1, data, 0),"inverse":container.noop,"data":data})) != null ? stack1 : "")
+    + "        </div>\n        <div class=\"col-md-6\">\n			<h1>Sheets Shared With Me</h1>\n\n"
+    + ((stack1 = helpers.each.call(depth0,(depth0 != null ? depth0.collab_sheets : depth0),{"name":"each","hash":{},"fn":container.program(1, data, 0),"inverse":container.noop,"data":data})) != null ? stack1 : "")
+    + "        </div>\n      </div>\n	</div>\n</div>\n";
 },"usePartial":true,"useData":true});
 templates['signin'] = template({"1":function(container,depth0,helpers,partials,data) {
     var helper;
 
-  return "      "
-    + container.escapeExpression(((helper = (helper = helpers.error || (depth0 != null ? depth0.error : depth0)) != null ? helper : helpers.helperMissing),(typeof helper === "function" ? helper.call(depth0 != null ? depth0 : {},{"name":"error","hash":{},"data":data}) : helper)))
+  return "        "
+    + container.escapeExpression(((helper = (helper = helpers.error || (depth0 != null ? depth0.error : depth0)) != null ? helper : helpers.helperMissing),(typeof helper === "function" ? helper.call(depth0,{"name":"error","hash":{},"data":data}) : helper)))
     + "\n";
 },"compiler":[7,">= 4.0.0"],"main":function(container,depth0,helpers,partials,data) {
     var stack1;
 
-  return "<div id=\"signin\" class=\"allCenter\">\n  <a href=\"#\" id=\"home-link\">Back to Home</a>\n  \n  <head>\n    <meta charset=\"utf-8\">\n    <meta http-equiv=\"X-UA-Compatible\" content=\"IE=edge\">\n    <meta name=\"viewport\" content=\"width=device-width, initial-scale=1\">\n    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->\n\n    <title>Signin Template for Bootstrap</title>\n\n\n\n  </head>\n\n\n  <h1>Sign in</h1>\n  <div class=\"error\">\n"
-    + ((stack1 = helpers["if"].call(depth0 != null ? depth0 : {},(depth0 != null ? depth0.error : depth0),{"name":"if","hash":{},"fn":container.program(1, data, 0),"inverse":container.noop,"data":data})) != null ? stack1 : "")
-    + "  </div>\n  <form id=\"signin-form\" class=\"spaceInputs\">\n    <div>Username: <input type=\"text\" name=\"username\" required /></div>\n    <div>Password: <input type=\"password\" name=\"password\" required /></div>\n    <input type=\"submit\" />\n  </form>\n</div>\n";
+  return "\n<div class=\"container\">\n    <div class=\"page-header\">\n      <h1>CoCompose</h1>\n    </div>\n    <a href=\"#\" id=\"home-link\">Back to Home</a>\n    <h1>Sign In</h1>\n    <div class=\"error\">\n"
+    + ((stack1 = helpers["if"].call(depth0,(depth0 != null ? depth0.error : depth0),{"name":"if","hash":{},"fn":container.program(1, data, 0),"inverse":container.noop,"data":data})) != null ? stack1 : "")
+    + "    </div>\n    <form id=\"signin-form\" class=\"form-group\">\n      <div>Username: <input type=\"text\" name=\"username\" class=\"form-control\" placeholder=\"Username\" required /></div> <br>\n      <div>Password: <input type=\"password\" name=\"password\" class=\"form-control\" placeholder=\"Password\" required /></div>\n      <br>\n      <input type=\"submit\" class=\"btn btn-info\"/>\n    </form>\n</div>\n\n";
 },"useData":true});
 })();
